4b91630eb48befed3bb551fbbb7148732eefcc98
nicolasarmenti <nicolasarmenti@gmail.com>
2021-12-10 20:38:08 -0300

    Agregados atributos al campo Name de los clientes

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,6 +1,10 @@
-﻿namespace Vidly.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models {
     public class Customer {
         public int Id { get; set; }
+		[Required]
+		[StringLength(255)]
         public string Name { get; set; }
 		public bool IsSubscribedToNewsLetter { get; set; }
 		public MembershipType MembershipType { get; set; }
