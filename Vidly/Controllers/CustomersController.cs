6ca33b70e8d4d4ca3e6f202f3e1b07f0c123f783
nicolasarmenti <nicolasarmenti@gmail.com>
2021-12-11 18:21:29 -0300

    Cambiado el método que trae clientes para que busque en la base

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,25 +5,34 @@ using System.Collections.Generic;
 
 namespace Vidly.Controllers {
 	public class CustomersController : Controller {
+		private ApplicationDbContext _Context;
+
+		public CustomersController() {
+			_Context = new ApplicationDbContext();
+		}
+
+		protected override void Dispose(bool disposing) {
+			base.Dispose(disposing);
+			_Context.Dispose();
+		}
+
 		public ViewResult Index() {
-			return View(GetCustomers());
+			//return View(GetCustomers());
+
+			var customers = _Context.Customers; //defered execution
+			//var customers = _Context.Customers.ToList(); //inmediate execution
+
+			return View(customers);
 		}
 
 		[Route("custoemrs/details/{id:range(1, 99999999)}")]
 		public ActionResult Details(int id) {
-			var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+			var customer = _Context.Customers.SingleOrDefault(c => c.Id == id); //inmediate execution
 
 			if (customer == null)
 				return HttpNotFound();
 
 			return View(customer);
-		}
-
-		private IEnumerable<Customer> GetCustomers() {
-			return new List<Customer> {
-				new Customer { Id = 1, Name = "John Smith" },
-				new Customer { Id = 2, Name = "Mary Williams" }
-			};	
 		}
 	}
 }
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,11 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models {
-    public class Customer {
-        public int Id { get; set; }
-		[Required]
-		[StringLength(255)]
-        public string Name { get; set; }
+	public class Customer {
+		public int Id { get; set; }
+		[Required] [StringLength(255)] public string Name { get; set; }
 		public bool IsSubscribedToNewsLetter { get; set; }
 		public MembershipType MembershipType { get; set; }
 		public byte MembershipTypeId { get; set; }
