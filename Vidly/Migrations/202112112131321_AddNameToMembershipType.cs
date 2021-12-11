f4454ed3a1fa492a36b8f764146b51724741efe3
nicolasarmenti <nicolasarmenti@gmail.com>
2021-12-11 18:34:10 -0300

    Agregada columna nombre a la tabla de membrecias

---
+++ Vidly/Migrations/202112112131321_AddNameToMembershipType.cs
@@ -0,0 +1,18 @@
+namespace Vidly.Migrations {
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameToMembershipType : DbMigration {
+        public override void Up() {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String(nullable: false, maxLength: 200));
+
+			Sql("UPDATE MembershipTypes SET Name = 'Pay As You Go' WHERE Id = 1");
+			Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE Id = 2");
+			Sql("UPDATE MembershipTypes SET Name = 'Quarterly' WHERE Id = 3");
+			Sql("UPDATE MembershipTypes SET Name = 'Yearly' WHERE Id = 4");
+		}
+        
+        public override void Down() {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,6 +1,9 @@
-﻿namespace Vidly.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models {
 	public class MembershipType {
 		public byte Id { get; set; }
+		[Required] [StringLength(200)] public string Name { get; set; }
 		public short SignUpFee { get; set; }
 		public byte DurationInMonths { get; set; }
 		public byte DiscountRate { get; set; }
