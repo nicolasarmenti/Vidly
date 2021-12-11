385227249bd377d5581110adc032ffe201aa6e57
nicolasarmenti <nicolasarmenti@gmail.com>
2021-12-11 19:08:47 -0300

    Agregado género, fecha de estreno, fecha de alta y stock a la tabla de películas

---
+++ Vidly/Migrations/202112112205053_AddGenre.cs
@@ -0,0 +1,39 @@
+namespace Vidly.Migrations {
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddGenre : DbMigration {
+        public override void Up() {
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 100),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Movies", "GenreId", c => c.Byte(nullable: false));
+            AddColumn("dbo.Movies", "ReleaseDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "DateAdded", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "Stock", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Movies", "GenreId");
+            AddForeignKey("dbo.Movies", "GenreId", "dbo.Genres", "Id", cascadeDelete: true);
+
+			Sql("INSERT INTO Genres(Id, Name) VALUES(1, 'Comedy')");
+			Sql("INSERT INTO Genres(Id, Name) VALUES(2, 'Action')");
+			Sql("INSERT INTO Genres(Id, Name) VALUES(3, 'Family')");
+			Sql("INSERT INTO Genres(Id, Name) VALUES(4, 'Drama')");
+			Sql("INSERT INTO Genres(Id, Name) VALUES(5, 'Romance')");
+		}
+        
+        public override void Down() {
+            DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "GenreId" });
+            DropColumn("dbo.Movies", "Stock");
+            DropColumn("dbo.Movies", "DateAdded");
+            DropColumn("dbo.Movies", "ReleaseDate");
+            DropColumn("dbo.Movies", "GenreId");
+            DropTable("dbo.Genres");
+        }
+    }
+}
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,6 +1,13 @@
-﻿namespace Vidly.Models {
+﻿using System;
+
+namespace Vidly.Models {
     public class Movie {
         public int Id { get; set; }
         public string Name { get; set; }
-    }
+		public Genre Genre { get; set; }
+		public byte GenreId { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public DateTime DateAdded { get; set; }
+		public byte Stock { get; set; }
+	}
 }
